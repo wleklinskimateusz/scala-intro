c32ab06875ff0cc0c0a4d266c7e65797ba7f97ca
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2022-07-03 17:17:39 +0200

    init scala tutorial

---
+++ hello-world/src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+
+val oldNumbers = List(1, 2, 3)
+val newNumbers = oldNumbers.map(_ * 2)
+
+
+
+@main def hello: Unit = 
+  println("Hello world!")
+  println(msg)
+  println(oldNumbers)
+
+def msg = "I was compiled by Scala 3. :)"
